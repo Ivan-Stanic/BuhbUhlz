c9835f301e0c921e925799d6a7f0d9f751167174
Ivan Stanic <ivan.stanic.or@gmail.com>
2018-05-28 19:00:19 -0500

    Bubble touch (sort of)

--- core/src/com/buhbuhlz/buhbuhlz/game/BuhbUhlz.java
+++ core/src/com/buhbuhlz/buhbuhlz/game/BuhbUhlz.java
@@ -1,13 +1,14 @@
 package com.buhbuhlz.buhbuhlz.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
-import com.badlogic.gdx.utils.Logger;
 import com.badlogic.gdx.utils.TimeUtils;
 import com.badlogic.gdx.utils.Timer;
 
@@ -15,33 +16,35 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import static java.lang.Math.floor;
+import static java.lang.Math.incrementExact;
 import static java.lang.Math.max;
 
 
-public class BuhbUhlz extends ApplicationAdapter {
+public class BuhbUhlz extends ApplicationAdapter implements ApplicationListener, InputProcessor {
 	SpriteBatch batch;
 	Texture background;
 	Random rand = new Random();
 	boolean bubbleTime = false;
 	double bubbleTimer = 3; //sec
+    double bubbleStDev = 2; // standard deviation for Gaussian distribution
 	double bubbleLife = 1.5; //sec
 	float startPortion = 10; //screen portion (10 = 10th of the screen) for the start bubble size
 	float maxPortion = 4; //screen portion (10 = 10th of the screen) for the max bubble size
-
-	Logger logger;
 
 	ArrayList<Integer> bubbleXs = new ArrayList<Integer>();
 	ArrayList<Integer> bubbleYs = new ArrayList<Integer>();
 	ArrayList<Integer> bubbleColors = new ArrayList<Integer>();
 	ArrayList<Long> bubbleCreationTime = new ArrayList<Long>();
+	ArrayList<Float> bubbleRs = new ArrayList<Float>(); // bubble radius (used for touch test)
+	ArrayList<Boolean> bubbleTouched = new ArrayList<Boolean>();
 	ShapeRenderer shapeRenderer;
 
 	public void startTimer() {
-		double bubbleDelay = (Math.abs(rand.nextGaussian()) * (-bubbleTimer)) + bubbleTimer;
+		double bubbleDelay = (Math.abs(rand.nextGaussian()) * (-bubbleStDev)) + bubbleTimer;
 		if (bubbleDelay < 0.1) {
 			bubbleDelay = 0.1;
 		}
-		//logger.info(String.valueOf(bubbleDelay));
+		//Gdx.app.log("bubble delay:", String.valueOf(bubbleDelay));
 		Timer.schedule(new Timer.Task()
 		{
 			@Override
@@ -60,13 +63,15 @@ public class BuhbUhlz extends ApplicationAdapter {
 		background = new Texture("bg_" + String.valueOf(randBg) + ".jpg");
 		shapeRenderer = new ShapeRenderer();
 
+        Gdx.input.setInputProcessor(this);
+
 		startTimer();
 
 	}
 
 	public void makeBubble(int i) {
-		float startY;
-		float startX;
+		float currentY;
+		float currentX;
 		float radius;
 		float color;
 		float screenWidth = Gdx.graphics.getWidth();
@@ -74,21 +79,24 @@ public class BuhbUhlz extends ApplicationAdapter {
 		float startRadius = screenWidth/startPortion;
 		float maxRadius = screenWidth/maxPortion;
 		if ((i == bubbleXs.size() && bubbleXs.size() > 0) || bubbleXs.size() == 0) {
-			startY = rand.nextInt(Gdx.graphics.getHeight());
-			startX = rand.nextInt(Gdx.graphics.getWidth());
+			currentY = rand.nextInt(Gdx.graphics.getHeight());
+			currentX = rand.nextInt(Gdx.graphics.getWidth());
 			radius = startRadius;
-			color = rand.nextInt(8);
+			color = rand.nextInt(6) + 1; //from 1 to 6 (no white (0) and black (7))
 			creationTime = TimeUtils.millis();
-			bubbleYs.add(Math.round(startY));
-			bubbleXs.add(Math.round(startX));
+			bubbleYs.add(Math.round(currentY));
+			bubbleXs.add(Math.round(currentX));
 			bubbleColors.add(Math.round(color));
 			bubbleCreationTime.add(creationTime);
+			bubbleTouched.add(false);
+			bubbleRs.add(radius);
 		} else {
-			startY = bubbleYs.get(i);
-			startX = bubbleXs.get(i);
+			currentY = bubbleYs.get(i);
+			currentX = bubbleXs.get(i);
             color = bubbleColors.get(i);
             creationTime = bubbleCreationTime.get(i);
 			radius = (float) (((TimeUtils.millis() - creationTime) * (maxRadius - startRadius))/(bubbleLife * 1000)) + startRadius;
+			bubbleRs.set(i, radius);
 		}
 		float R = (float) floor(color/4);
 		float G = (float) floor((color % 4)/2);
@@ -97,17 +105,17 @@ public class BuhbUhlz extends ApplicationAdapter {
 		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 		shapeRenderer.begin(ShapeType.Filled);
 		shapeRenderer.setColor(R, G, B, .5f);
-		shapeRenderer.circle(startX, startY, radius);
-		shapeRenderer.setColor(1f, 1f, 1f, .08f);
-		shapeRenderer.circle(startX - radius/3, startY + radius/3, (float) (radius * 0.5));
-		shapeRenderer.setColor(1f, 1f, 1f, .08f);
-		shapeRenderer.circle(startX - radius/3, startY + radius/3, (float) (radius * 0.4));
-		shapeRenderer.setColor(1f, 1f, 1f, .08f);
-		shapeRenderer.circle(startX - radius/3, startY + radius/3, (float) (radius * 0.3));
-		shapeRenderer.setColor(1f, 1f, 1f, .08f);
-		shapeRenderer.circle(startX - radius/3, startY + radius/3, (float) (radius * 0.2));
-        shapeRenderer.setColor(1f, 1f, 1f, .08f);
-        shapeRenderer.circle(startX - radius/3, startY + radius/3, (float) (radius * 0.1));
+		shapeRenderer.circle(currentX, currentY, radius);
+		shapeRenderer.setColor(1f, 1f, 1f, .06f);
+		shapeRenderer.circle(currentX - radius/3, currentY + radius/3, (float) (radius * 0.5));
+		shapeRenderer.setColor(1f, 1f, 1f, .06f);
+		shapeRenderer.circle(currentX - radius/3, currentY + radius/3, (float) (radius * 0.4));
+		shapeRenderer.setColor(1f, 1f, 1f, .06f);
+		shapeRenderer.circle(currentX - radius/3, currentY + radius/3, (float) (radius * 0.3));
+		shapeRenderer.setColor(1f, 1f, 1f, .06f);
+		shapeRenderer.circle(currentX - radius/3, currentY + radius/3, (float) (radius * 0.2));
+        shapeRenderer.setColor(1f, 1f, 1f, .06f);
+        shapeRenderer.circle(currentX - radius/3, currentY + radius/3, (float) (radius * 0.1));
         shapeRenderer.end();
 	}
 
@@ -121,11 +129,13 @@ public class BuhbUhlz extends ApplicationAdapter {
 
 		int i = 0;
 		while (i < bubbleXs.size()) {
-			makeBubble(i);
+            if (! bubbleTouched.get(i)) {
+                makeBubble(i);
+            }
 			i++;
 		}
 		if (bubbleTime) {
-			makeBubble(i);
+            makeBubble(i);
 			bubbleTime = false;
 			startTimer();
 		}
@@ -136,6 +146,8 @@ public class BuhbUhlz extends ApplicationAdapter {
 				bubbleYs.remove(i);
 				bubbleColors.remove(i);
 				bubbleCreationTime.remove(i);
+				bubbleTouched.remove(i);
+				bubbleRs.remove(i);
 				i--;
 			}
 			i++;
@@ -148,5 +160,62 @@ public class BuhbUhlz extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		int i = 0;
+        float radius;
+        float currentY;
+        float currentX;
+        while (i < bubbleXs.size()) {
+            if (! bubbleTouched.get(i)) {
+                radius = bubbleRs.get(i);
+                currentY = bubbleYs.get(i);
+                currentX = bubbleXs.get(i);
+                if (Math.sqrt(Math.pow(screenX - currentX, 2) + Math.pow(screenY - currentY, 2)) <= radius) {
+                    //Gdx.app.log("Touched:", "Catch!!!");
+                    bubbleTouched.set(i, true);
+                    i = bubbleXs.size(); //exit loop
+                }
+            }
+            i++;
+        }
+        return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
 	}
 }
